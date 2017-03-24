da6e19abd471bca8779a93723385b417ab4a87eb
cphotiadou <christiana.photiadou@knmi.nl>
2017-03-24 13:30:45 +0000

    remove importFrom

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,5 @@
 #' @importFrom data.table fread
 #' @importFrom utils write.table
 #' @import futile.logger
-#' @importFrom knmitransformer droogte_berekening_KNMI14
+#' @import knmitransformer
 NULL
