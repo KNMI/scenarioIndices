eefacce34cb1311c338c73d123cc902396a19376
cphotiadou <christiana.photiadou@knmi.nl>
2017-06-22 10:14:57 +0000

    precipitation indices

--- R/precipitationIndices.R
+++ R/precipitationIndices.R
@@ -18,32 +18,41 @@
 #'                   then the indices are calculated for the reference period 1981-2010
 #' @param horizon    time horizon ( DEFAULT=2030, 2050, 2085). If horizon is not one of the 3
 #'                   then the indices are calculated for the reference period 1981-2010
-#' @param season     season (0= year, 1=winter, 2=spring, 3=summer, 4=autumn)
 #' @param subscenario subscenario for extreme precipitation
 #' ("lower", "centr" (=DEFAULT), "upper")
 #' @param ofile      (DEFAULT=NA) Name of the output file to write the indices to.
 #'                   Format is similar to input without the 5 first lines
 #' @export
 PrecipThreshIndices<- function(input, threshold, scenario = NA,
-                          horizon = NA, season, subscenario, ofile = NA) {
+                          horizon = NA, subscenario, ofile = NA) {
 
+
+  StationSub <- as.character(fread(system.file("refdata","P102.txt", package = "scenarioIndices"))$V1)
 
   # calcualte index for reference; else...
-  if (!scenario %in% c("GL","GH","WL","WH") && horizon !=c(2030,2050,2085)){
-    input <-  knmitransformer:::ReadInput("rr",
-                                          system.file("refdata","KNMI14____ref_rr___19810101-20101231_v3.2.txt",
-                                                      package="knmitransformer"))$obs
-  } else {
-    input <- TransformPrecip(input = input, ofile=NA, scenario=scenario,
+  # if (!scenario %in% c("GL","GH","WL","WH") && horizon !=c(2030,2050,2085)){
+  #   input <-  knmitransformer:::ReadInput("rr",
+  #                                         system.file("refdata","KNMI14____ref_rrcentr___19810101-20101231_v3.2.txt",
+  #                                                     package="knmitransformer"))$obs
+  #   input <- input[,StationSub]
+  # } else {
+    input <- knmitransformer::TransformPrecip(input = input, ofile=NA, scenario=scenario,
                            horizon=horizon, subscenario = subscenario)
-    input <- input[-(1:5), ]
+   stationID         <- input[(1)]
+    names(input) <- as.character(stationID)
+    input <- input[-(1:5),StationSub, with=FALSE]
 
-  }
+  # }
 
   input <- as.data.frame(input)
 
-  tabel <- as.data.frame(matrix(NA,5,ncol(input)))
+  products <- data.frame("sd.Psum"=1,"Psum"=1)
+
+  threshold <- c(0.1,0.3,1.0,5.0,10.0,15.0,20.0,30.0)
+
+  tabel <- as.data.frame(matrix(NA,5*(length(threshold)+sum(products)),ncol(input)))
   names(tabel) <- names(input)
+
 
   #Seasons
   mm <- (input[,1]%/%100)%%100
@@ -51,21 +60,52 @@ PrecipThreshIndices<- function(input, threshold, scenario = NA,
   yy <-  input[,1]%/%10000
   wy <- ifelse(mm<12,yy,yy+1)
 
-  if(season=="year"){
-    id  <- 1:length(yy)
-    idy <- yy
-  } else {
-    if(season=="winter"){
-      id  <- which(ss==1 & wy > min(wy) & wy < max(wy))
-      idy <- wy[id]
+  i=0
+
+  # seasonal variables
+  for(season in 0:4) {
+    if(season==0) {
+      id  <- 1:length(yy)
+      idy <- yy
     } else {
-      id  <- which(ss==season)
-      idy <- yy[id]
+      if(season==1) {
+        id  <- which(ss==1 & wy > min(wy) & wy < max(wy))
+        idy <- wy[id]
+      } else {
+        id  <- which(ss==season)
+        idy <- yy[id]
+      }
     }
-  }
 
-      tabel[i,-1] <- apply(rr[id,-1]>=threshold,2,sum) / length(unique(yy))
+    if(length(threshold)>0) {
+      for(j in 1:length(threshold)) {
+        i=i+1
+        dname <- paste("N_",season, "_",threshold[j],"mm",sep="")
+        tabel[i, 1] <- paste(dname,substr("        ",1,8-nchar(dname)))
+        tabel[i,-1] <- apply(input[id,-1]>=threshold[j],2,sum) / length(unique(yy))
+      }
+    }
 
+    prod="Psum"   # mean seizoenssom RR
+    if(products[prod]==1) {
+      dname=paste(prod,"_",season,sep="")
+      i=i+1
+      tabel[i, 1] <- paste(dname,substr("        ",1,8-nchar(dname)))
+      X           <- aggregate(input[id,-1],by=list(idy),  sum)
+      tabel[i,-1] <- apply(X[,-1]       , 2          , mean)
+    }
+
+    prod="sd.Psum"   # sd seizoenssom RR
+    if(products[prod]==1) {
+      dname=paste(prod,"_",season,sep="")
+      i=i+1
+      tabel[i, 1] <- paste(dname,substr("        ",1,8-nchar(dname)))
+      X           <- aggregate(input[id,-1],by=list(idy),  sum)
+      tabel[i,-1] <- apply(X[,-1]       , 2          ,   sd)
+    }
+
+  } # end seasonal variables
+  return(tabel)
 }
 
 
