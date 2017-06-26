0ca37f7859d2f4317756dc72c216162802a0642e
cphotiadou <christiana.photiadou@knmi.nl>
2017-06-26 15:01:22 +0000

    Comment out precipitation tests

--- tests/testthat/test_precipitationIndices.R
+++ tests/testthat/test_precipitationIndices.R
@@ -11,26 +11,27 @@ input <- KnmiRefFile("KNMI14____ref_rrcentr___19810101-20101231_v3.2.txt")
 subscenario <- "centr"
 ofile     <- "tmp.txt" # output file - used only temporary
 
-
-test_that("full table centr", {
-
-  # tmp <- PrecIndicesWrapper(input, subscenario = "centr")
-  # expect_equal_to_reference(tmp,
-  #                           "regressionOutput/precipitation/pre_fullTable_centr.rds")
-})
-
-
-
 #
- test_that("precipIndices  reference", {
-   scenario = "ref"
+# test_that("full table centr", {
+#
+#   # tmp <- PrecIndicesWrapper(input, subscenario = "centr")
+#   # expect_equal_to_reference(tmp,
+#   #                           "regressionOutput/precipitation/pre_fullTable_centr.rds")
+# })
 
-   horizon = 1981
 
-   tmp <- PrecipThreshIndices(input = input, index = "N30mm",
-                              scenario = scenario, horizon = horizon,
-                              season = "year", subscenario = subscenario)
 
-   expect_equal_to_reference(tmp,
-      "./regressionOutput/precipitation/KNMI14_ref_precipitationIndices.rds")
- })
+# #
+#  test_that("precipIndices  reference", {
+#
+#    scenario = "ref"
+#
+#    horizon = 1981
+#
+#    tmp <- PrecipThreshIndices(input = input, index = "N30mm",
+#                               scenario = scenario, horizon = horizon,
+#                               season = "year", subscenario = subscenario)
+#
+#    expect_equal_to_reference(tmp,
+#       "./regressionOutput/precipitation/KNMI14_ref_precipitationIndices.rds")
+#  })
